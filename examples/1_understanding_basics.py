d21d7d4bc6af3dc31d0fdbd698caba30e5ecc74e
Raul C. Sîmpetru <raul.simpetru@fau.de>
2025-03-09 19:48:12 +0100

    Refactor EMG data loading and visualization; enhance filter application and output representation checks

--- examples/1_understanding_basics.py
+++ examples/1_understanding_basics.py
@@ -5,6 +5,13 @@ Package basics
 This example is a brief introduction to the basic functionalities of the package.
 """
 
+import os
+import pickle as pkl
+import matplotlib.pyplot as plt
+import numpy as np
+from myoverse.datatypes import EMGData
+from scipy.signal import butter
+from myoverse.datasets.filters.temporal import SOSFrequencyFilter
 
 # %%
 # Loading data
@@ -12,12 +19,15 @@ This example is a brief introduction to the basic functionalities of the package
 # First we load the EMG example data and convert it to a MyoVerse Data object.
 # The Data object is the primary component of the package, designed to store data and apply filters.
 # The only required parameter is the sampling frequency of the data and the data itself.
-import pickle as pkl
-from myoverse.datatypes import EMGData
 
-with open("data/emg.pkl", "rb") as f:
-    emg_data =  {k: EMGData(v, sampling_frequency=2044) for k, v in pkl.load(f).items()}
+# Get the path to the data file
+SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
+data_path = os.path.join(SCRIPT_DIR, "data", "emg.pkl")
 
+with open(data_path, "rb") as f:
+    emg_data = {k: EMGData(v, sampling_frequency=2044) for k, v in pkl.load(f).items()}
+
+print("EMG data loaded successfully:")
 print(emg_data)
 
 # %%
@@ -27,6 +37,7 @@ print(emg_data)
 # In the following we will only look at task 1 to explain the filtering functionalities.
 task_one_data = emg_data["1"]
 
+print("\nTask 1 data details:")
 print(task_one_data)
 
 # %%
@@ -41,18 +52,21 @@ print(task_one_data)
 # The attribute **is_chunked** is a dictionary that stores if the data of a particular representation is chunked or not.
 #
 # The attribute **output_representations** is a dictionary that stores the representation that will be outputted by the dataset pipeline.
-print(task_one_data.input_data)
+print("\nRaw input data shape:")
+print(task_one_data.input_data.shape)
 
 # %%
 # Plotting the raw data
 # ---------------------
 # We can plot the raw data using matplotlib.
-import matplotlib.pyplot as plt
-import numpy as np
 
+# Create a figure with better dimensions for visualization
+plt.figure(figsize=(12, 6))
+
+# Get the raw EMG data
 raw_emg = task_one_data.input_data
 
-# set plt font size
+# Set plt font size for better readability
 plt.rcParams.update({"font.size": 14})
 
 for channel in range(raw_emg.shape[0]):
@@ -66,28 +80,42 @@ plt.xticks(
 )
 plt.xlabel("Time (s)")
 
+# Add a grid for better readability
+plt.grid(True, linestyle='--', alpha=0.7)
+
 plt.tight_layout()
 plt.show()
 
 # %%
 # Attributes of the EMGData object
 # --------------------------------
-# Any Data object, of which EMGData is inheriting from, posses a processed_representations attribute where filtered data  will be stored.
+# Any Data object, of which EMGData is inheriting from, possesses a processed_representations attribute where filtered data will be stored.
 #
 # .. note :: We refer to a filtered data as a representation.
 #
 # At the beginning this attribute only contains the raw data with the key "Input".
-print(task_one_data.processed_representations)
+print("\nInitial processed representations:")
+print(task_one_data.processed_representations.keys())
 
 # %%
 # Applying a filter
 # -----------------
 # The EMGData object has a method called **apply_filter** that applies a filter to the data.
-# For example, we can apply a 4th order 20 HZ lowpass filter to the data.
-from scipy.signal import butter
-from myoverse.datasets.filters.temporal import SOSFrequencyFilter
+# For example, we can apply a 4th order 20 Hz lowpass filter to the data.
 
-sos_filter_coefficients = butter(4, 20, "lowpass", output="sos", fs=2044)
+# Define the filter parameters
+FILTER_ORDER = 4
+CUTOFF_FREQ = 20  # Hz
+SAMPLING_FREQ = 2044  # Hz
+
+# Create the filter coefficients using a Butterworth filter design
+sos_filter_coefficients = butter(
+    FILTER_ORDER, 
+    CUTOFF_FREQ, 
+    "lowpass", 
+    output="sos", 
+    fs=SAMPLING_FREQ
+)
 
 # %%
 # Creating the filter
@@ -97,8 +125,12 @@ sos_filter_coefficients = butter(4, 20, "lowpass", output="sos", fs=2044)
 # Since we want to have the result of the filter as an output representation, we set the parameter **is_output** to True.
 # Further having the user specify this parameter forces them to think about the data they are working with.
 lowpass_filter = SOSFrequencyFilter(
-    sos_filter_coefficients, is_output=True, name="Lowpass", input_is_chunked=False
+    sos_filter_coefficients=sos_filter_coefficients, 
+    is_output=True, 
+    name="Lowpass", 
+    input_is_chunked=False
 )
+print("\nFilter configuration:")
 print(lowpass_filter)
 
 # %%
@@ -106,10 +138,11 @@ print(lowpass_filter)
 # -------------------
 # To apply the filter we call the apply_filter method on the EMGData object.
 task_one_data.apply_filter(
-    lowpass_filter, representation_to_filter="Last"
+    lowpass_filter, representations_to_filter=["Input"]
 )
-
-print(task_one_data.processed_representations)
+print("\nFilter applied successfully!")
+print("Available processed representations:")
+print(task_one_data.processed_representations.keys())
 
 # %%
 # Accessing the filtered data
@@ -119,11 +152,43 @@ print(task_one_data.processed_representations)
 #
 # In case you do not want to index using the filter sequence name, you can retrieve the last processed data by indexing with "Last".
 
+print("\nFiltered data details:")
 print(task_one_data)
 
-print(
-    np.allclose(
-        task_one_data.output_representations["Lowpass"],
-        task_one_data["Last"],
-    )
+# Verify that both methods of accessing the filtered data return the same result
+identical = np.allclose(
+    task_one_data.output_representations["Lowpass"],
+    task_one_data["Last"],
 )
+print(f"\nLowpass and Last point to the same data: {identical}")
+
+# %%
+# Visualizing the difference between raw and filtered data
+# -------------------------------------------------------
+# Let's plot both the raw and filtered data to see the effect of the lowpass filter
+
+plt.figure(figsize=(12, 8))
+
+# For visualization clarity, let's plot only one channel
+channel_to_plot = 0
+filtered_emg = task_one_data["Lowpass"]
+
+# Plot raw EMG for the selected channel
+plt.subplot(2, 1, 1)
+plt.plot(raw_emg[channel_to_plot], color="blue", label="Raw EMG")
+plt.title(f"Raw EMG - Channel {channel_to_plot+1}")
+plt.ylabel("Amplitude (a.u.)")
+plt.grid(True, linestyle='--', alpha=0.7)
+plt.legend()
+
+# Plot filtered EMG for the selected channel
+plt.subplot(2, 1, 2)
+plt.plot(filtered_emg[channel_to_plot], color="red", label="Lowpass Filtered EMG")
+plt.title(f"Lowpass Filtered EMG (Cutoff: {CUTOFF_FREQ} Hz) - Channel {channel_to_plot+1}")
+plt.ylabel("Amplitude (a.u.)")
+plt.xlabel("Time (s)")
+plt.grid(True, linestyle='--', alpha=0.7)
+plt.legend()
+
+plt.tight_layout()
+plt.show()
